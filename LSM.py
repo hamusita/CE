41b6b7184ea710919226f8851ab19f5926235e30
hamusita <meazero965@gmail.com>
2018-07-03 10:52:33 +0900

    add LSM

---
+++ LSM.py
@@ -0,0 +1,14 @@
+
+def input_num():
+    n = int(input("補間点の個数を入力:n = "))
+    print("続いて補間点の座標を入力してください")
+    li = [[],[]]
+    for i in range(n):
+        li[0].append(float(input("x%d = " % (i + 1))))
+        li[1].append(float(input("y%d = " % (i + 1))))
+    c = []
+    c.append(float(input("全区間の左端点における１次微分係数は？ : C1 = ")))
+    c.append(float(input("全区間の右端点における１次微分係数は？ : Cn = ")))
+    print(li, c)
+    print("Ok. 入力を受け付けました")
+    return li, c
